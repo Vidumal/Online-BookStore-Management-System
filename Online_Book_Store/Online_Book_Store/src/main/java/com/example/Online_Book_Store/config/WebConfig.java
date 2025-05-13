fd3510d75695001e18bec13e8a1aa701bc10cddb
Punsith Vidumal <punsithvidumal@gmail.com>
2025-05-13 14:44:02 +0530

    Create WebConfig.java

---
+++ Online_Book_Store/Online_Book_Store/src/main/java/com/example/Online_Book_Store/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.example.Online_Book_Store.config;
+
+import com.example.Online_Book_Store.constants.Constant;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    // Path to the external folder where images will be stored
+    private static final String EXTERNAL_IMAGE_DIR = Constant.EXTERNAL_IMAGE_DIR;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Map /images/** to the external directory D:/Out Source Projects/online-book-store/data/images/
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations(EXTERNAL_IMAGE_DIR);
+    }
+}
+
