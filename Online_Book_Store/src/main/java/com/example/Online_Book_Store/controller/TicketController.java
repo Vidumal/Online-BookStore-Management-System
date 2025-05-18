4f1e586d60c19ec9d5635caf542948f546f6ed04
Punsith Vidumal <punsithvidumal@gmail.com>
2025-05-19 00:55:00 +0530

    create ticket controller file and Service file

--- Online_Book_Store/src/main/java/com/example/Online_Book_Store/controller/TicketController.java
+++ Online_Book_Store/src/main/java/com/example/Online_Book_Store/controller/TicketController.java
@@ -1,4 +1,13 @@
 package com.example.Online_Book_Store.controller;
 
+import com.example.Online_Book_Store.model.Ticket;
+import com.example.Online_Book_Store.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
 public class TicketController {
 }
