25bf5bf3fce0026bb80bfeb3ee2c75daddb29b85
IT24102736 <>
2025-05-19 13:32:24 +0530

    Update Book.java

--- Online_Book_Store/src/main/java/com/example/Online_Book_Store/model/Book.java
+++ Online_Book_Store/src/main/java/com/example/Online_Book_Store/model/Book.java
@@ -1,6 +1,24 @@
 package com.example.Online_Book_Store.model;
 
 public class Book {
+    private String id;
+    private String name;
+    private String author;
+    private int quantity;
+    private double price;
+    private String category;
+    private String imagePath; // Path like "data/images/book1.jpg"
 
-    //comment
+    public Book() {}
+
+    public Book(String id, String name, String author,
+                int quantity, double price, String category, String imagePath) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.quantity = quantity;
+        this.price = price;
+        this.category = category;
+        this.imagePath = imagePath;
+    }
 }
