b633207000b475f51dbe8ce6c6fc766e445e040f
Manudi2004 <manudimithara@gmail.com>
2025-05-19 19:52:36 +0530

    Update Offer.java

--- Online_Book_Store/src/main/java/com/example/Online_Book_Store/model/Offer.java
+++ Online_Book_Store/src/main/java/com/example/Online_Book_Store/model/Offer.java
@@ -17,4 +17,35 @@ public class Offer {
         this.category = category;
         this.imagePath = imagePath;
     }
+    // Getters and Setters
+    public Integer getId() {
+        return id; }
+    public void setId(Integer id) {
+        this.id = id; }
+
+    public String getDescription() {
+        return description; }
+    public void setDescription(String description) {
+        this.description = description; }
+
+    public float getSave() {
+        return save; }
+    public void setSave(float save) {
+        this.save = save; }
+
+    public String getCategory() {
+        return category; }
+    public void setCategory(String category) {
+        this.category = category; }
+
+    public String getImagePath() {
+        return imagePath; }
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Description: " + description + ", Save: " + save +
+                ", Category: " + category + ", ImagePath: " + imagePath;
+    }
 }
