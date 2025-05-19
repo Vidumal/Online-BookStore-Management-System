065e53b403cbd90df814d3441332a3e009217bc5
thabith12 <m.t2004b.t@gmail.com>
2025-05-19 21:10:06 +0530

    first commit

--- Online_Book_Store/src/main/java/com/example/Online_Book_Store/constants/Constant.java
+++ Online_Book_Store/src/main/java/com/example/Online_Book_Store/constants/Constant.java
@@ -1,6 +1,7 @@
 package com.example.Online_Book_Store.constants;
 
 public class Constant {
+    
     public final static String IMAGE_DIR = "C:\\Users\\USER\\Desktop\\online-book-store\\online-book-store\\online-book-store\\online-book-store\\data\\images";
     public final static String EXTERNAL_IMAGE_DIR = "file:C:/Users/USER/Desktop/online-book-store/online-book-store/online-book-store/online-book-store/data/images/";
 }
