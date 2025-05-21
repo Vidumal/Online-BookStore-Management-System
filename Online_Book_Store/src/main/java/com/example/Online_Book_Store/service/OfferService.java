6498a4930335e12385becff685ec1200c13260df
Manudi2004 <manudimithara@gmail.com>
2025-05-21 20:55:09 +0530

    Update OfferService.java

--- Online_Book_Store/src/main/java/com/example/Online_Book_Store/service/OfferService.java
+++ Online_Book_Store/src/main/java/com/example/Online_Book_Store/service/OfferService.java
@@ -7,4 +7,37 @@ import java.io.*;
 import java.util.*;
 
 @Service
-public class OfferService {}
+public class OfferService {
+
+    //LINKED LIST
+    private Node head;
+    Offer offer = new Offer();
+    // Auto-increment ID tracker for assigning new offer IDs
+    private int nextId = 1;
+
+    // Flag to ensure offers are only loaded from file once
+    private boolean offersLoaded = false;
+
+    private class Node {
+        Offer offer;
+        Node next;
+
+        public Node(Offer offer) {
+            this.offer = offer;
+            this.next = null;
+        }
+    }
+
+    public void loadOffers() throws IOException {
+        if (offersLoaded) return;
+
+        File file = new File("data/offers.txt");
+
+        if (!file.exists()) {
+            System.out.println("No offers file found. Skipping load.");
+            return;
+        }
+
+
+    }
+}
