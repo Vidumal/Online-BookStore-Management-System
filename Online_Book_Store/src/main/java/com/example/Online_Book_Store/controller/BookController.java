83790854f4a7ea46b07563d0db316b08b7f7e231
Sohan2004 <sohanrathnayake466@gmail.com>
2025-05-21 14:00:46 +0530

    create bookcontroller and service

--- Online_Book_Store/src/main/java/com/example/Online_Book_Store/controller/BookController.java
+++ Online_Book_Store/src/main/java/com/example/Online_Book_Store/controller/BookController.java
@@ -1,4 +1,21 @@
 package com.example.Online_Book_Store.controller;
 
+import com.example.Online_Book_Store.constants.Constant;
+import com.example.Online_Book_Store.model.Book;
+import com.example.Online_Book_Store.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.List;
+
+@Controller
+@RequestMapping("/books")
+
 public class BookController {
 }
