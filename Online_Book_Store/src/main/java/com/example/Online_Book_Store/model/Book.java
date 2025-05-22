33988580fef35d0417316c1ec32362d81455b79f
Sohan20040117 <sohanrathnayake466@gmail.com>
2025-05-22 17:32:09 +0530

    Update Book.java

--- Online_Book_Store/src/main/java/com/example/Online_Book_Store/model/Book.java
+++ Online_Book_Store/src/main/java/com/example/Online_Book_Store/model/Book.java
@@ -32,33 +32,41 @@ public class Book {
         return name;
     }
     public void setName(String name) {
+
         this.name = name;
     }
     public String getAuthor() {
         return author;
     }
     public void setAuthor(String author) {
+
         this.author = author;
     }
     public int getQuantity() {
+
         return quantity;
     }
     public void setQuantity(int quantity) {
+
         this.quantity = quantity;
     }
     public double getPrice() {
+
         return price;
     }
     public void setPrice(double price) {
+
         this.price = price;
     }
     public String getCategory() {
+
         return category;
     }
     public void setCategory(String category) {
         this.category = category;
     }
     public String getImagePath() {
+
         return imagePath;
     }
     public void setImagePath(String imagePath) {
