c7194d5493f815f1299dcb4668a3db17a07585ae
Punsith Vidumal <punsithvidumal@gmail.com>
2025-05-22 16:00:35 +0530

    Create PaymentService.java

---
+++ Online_Book_Store/src/main/java/com/example/Online_Book_Store/service/PaymentService.java
@@ -0,0 +1,10 @@
+package com.example.Online_Book_Store.service;
+import com.example.Online_Book_Store.model.Payment;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class PaymentService {
+}
